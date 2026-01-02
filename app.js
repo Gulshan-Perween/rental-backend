9fd769e6fd24336dba5a31b5c7dc043fce118cee
Gulshan-Perween <perweengulshan56@gmail.com>
2026-01-02 14:06:14 +0530

    kuch v

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
 import vehicleRoutes from "./routes/vehicleRoutes.js";
 import bookingRoutes from "./routes/bookingRoutes.js";
+import { get } from "mongoose";
+import Vehicle from "./models/Vehicle.js";
+import { ownerBookings } from "./controllers/bookingController.js";
 
 dotenv.config();
 
@@ -13,7 +16,16 @@ dotenv.config();
 connectDB();
 
 const app = express();
-
+const ownerVehicles = async (req, res) => {
+  try {
+    const vehicles = await Vehicle.find({ owner: req.user._id });
+    console.log("OWNER VEHICLES 👉", vehicles);
+    res.json(vehicles);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+// ownerVehicles();
 app.use(
   cors({
     origin: "http://localhost:5173",
